019e2558c069754d2e4383ffb781a127e7db87db
vinod palne <vinodpalne@gmail.com>
2018-11-18 13:56:37 +0530

    First Commit

---
+++ AutoCompleteTextBoxDemo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoCompleteTextBoxDemo.Models;
+namespace AutoCompleteTextBoxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        CountryDbContext objCountryDbContext = new CountryDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetCountryList()
+        {
+            var vData = from c in objCountryDbContext.Country
+                        select c.CountryName;
+            
+            return Json(vData.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AutoCompleteTextBoxDemo/Models/CountryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AutoCompleteTextBoxDemo.Models
+{
+    public class CountryDbContext:DbContext
+    {
+        public DbSet<Country> Country { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<CountryDbContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
